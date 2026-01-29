dad19507f1a7c47dfd11e1c9afe9e464117a7f3f
Manar <yourEmail@example.com>
2026-01-29 22:10:23 +0200

    add DoctorHome and DoctorProfile

--- lib/widgets/customCardWidget.dart
+++ lib/widgets/customCardWidget.dart
@@ -2,30 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:t3afy/constants.dart';
 
 class Customcardwidget extends StatelessWidget {
-  Customcardwidget({super.key, required this.child,required this.ontap});
-final Widget child;
-VoidCallback ontap;
+  // إضافة width و height كـ متغيرات
+  final Widget child;
+  final VoidCallback ontap;
+  final double width;
+  final double height;
+
+  // تعديل الكونستركتور عشان ياخدهم
+  Customcardwidget({
+    super.key,
+    required this.child,
+    required this.ontap,
+    this.width = 150,   // قيمة افتراضية
+    this.height = 130,  // قيمة افتراضية
+  });
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: ontap,
       child: Container(
-              height: 130,
-              width: 150,
-              decoration: BoxDecoration(
-                color: KPrimaryColor,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius:20,
-                    spreadRadius: 2,
-                    offset: Offset(0, 8),
-                  )
-                  ],
-              ),
-              child: child
+        height: height, // استخدم القيمة اللي اتعدت
+        width: width,   // استخدم القيمة اللي اتعدت
+        decoration: BoxDecoration(
+          color: KPrimaryColor,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 20,
+              spreadRadius: 2,
+              offset: Offset(0, 8),
             ),
+          ],
+        ),
+        child: child,
+      ),
     );
   }
 }
