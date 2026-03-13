84f3067482dbf422eda0e75ade0346bd911c6fac
Mostafa Kamal <almostafaabdo09@gmail.com>
2026-03-13 16:41:31 +0200

    update progress page by add legend to multi line chart

--- lib/widgets/card_in_progress.dart
+++ lib/widgets/card_in_progress.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:t3afy/constants.dart';
 import 'package:t3afy/pages/feelings_insights_page.dart';
 
 class ProgressCard extends StatelessWidget {
@@ -10,7 +11,9 @@ final()  onTap;
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: KPrimaryColor,
   child: ListTile(
+    
     leading: icon,
     title: Text(title,style: TextStyle(fontSize: 13),),
     subtitle: Text(subtitle,style: TextStyle(fontSize: 11),),
--- lib/widgets/recovery_score_indecator.dart
+++ lib/widgets/recovery_score_indecator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:t3afy/constants.dart';
 
 class RecoveryScoreIndicator extends StatelessWidget {
   final double score;
@@ -8,32 +9,48 @@ class RecoveryScoreIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircularPercentIndicator(
-      
-      radius: 100,
-      lineWidth: 14,
-      percent: score,
-      animation: true,
-      animationDuration: 1200,
-      circularStrokeCap: CircularStrokeCap.round,
-      backgroundColor: Colors.grey.shade300,
-      progressColor: Colors.green,
-      center: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "${(score * 100).toInt()}%",
-            style: const TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+  decoration: BoxDecoration(
+    color: KPrimaryColor,
+    borderRadius: BorderRadius.circular(20),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.1),
+        blurRadius: 20,
+        spreadRadius: 2,
+        offset: Offset(0, 8), // shadow تحت
+      ),
+    ],
+  ),
+      child: CircularPercentIndicator(
+        header: Text("Daily recovery Score",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+        radius: 100,
+        lineWidth: 14,
+        percent: score,
+        animation: true,
+        animationDuration: 1200,
+        circularStrokeCap: CircularStrokeCap.round,
+        backgroundColor: Colors.grey.shade300,
+        progressColor: Colors.green,
+        center: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${(score * 100).toInt()}%",
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          const Text(
-            "Recovery",
-            style: TextStyle(fontSize: 14),
-          )
-        ],
+            const SizedBox(height: 4),
+            const Text(
+              "Recovery",
+              style: TextStyle(fontSize: 14),
+            )
+          ],
+        ),
       ),
     );
   }
